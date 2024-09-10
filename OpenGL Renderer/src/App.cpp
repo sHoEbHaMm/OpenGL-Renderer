1ed03488116cc10a1231b9d61f35cd11e425c217
Shubham More <shubham.a.more20@gmail.com>
2024-09-10 14:36:41 -0700

    added GLM

--- OpenGL Renderer/src/App.cpp
+++ OpenGL Renderer/src/App.cpp
@@ -1,5 +1,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <math.h>
 #include "../Shader.h"
 #define STB_IMAGE_IMPLEMENTATION
